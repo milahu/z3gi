0b60bb22e3c815b0c8b83f43fe46f051b0d4089c
Rick Smetsers <r.smetsers@cs.ru.nl>
2017-06-12 22:06:24 +0200

    Added (restricted) register automaton definition.

--- z3gi/define.py
+++ z3gi/define.py
@@ -7,7 +7,7 @@ Output = z3.DeclareSort('Output')
 Element = z3.DeclareSort('Element')
 
 FSM = collections.namedtuple('FSM', 'start transition output')
-
+RA = collections.namedtuple('RA', 'start transition output registers set select store')
 
 def DFA(name=''):
     if name:
@@ -31,6 +31,24 @@ def MealyMachine(name=''):
     return FSM(start=z3.Const('{0}start'.format(name), State),
                transition=z3.Function('{0}transition'.format(name), State, Input, State),
                output=z3.Function('{0}output'.format(name), State, Input, Output))
+
+def RegisterAutomaton(name='', registers=1):
+    if name:
+        name = name + '_'
+
+    Register = z3.Datatype('{0}Register'.format(name))
+    registers = [Register.declare('fresh/perp')]
+    for i in range(1, registers+1):
+        registers.append(Register.declare('register{0}'.format(i)))
+    Register.create()
+
+    return RA(start=z3.Const('{0}start'.format(name), State),
+              transition=z3.Function('{0}transition'.format(name), State, Register, State),
+              output=z3.Function('{0}output'.format(name), State, z3.BoolSort()),
+              registers=registers,
+              set=z3.Function('{0}set'.format(name), State, Register, z3.BoolSort()),
+              select=z3.Function('{0}select'.format(name), State, Input, Register),
+              store=z3.Function('{0}store'.format(name), State, Input, Register))
 
 
 def StateMapper(name=''):
