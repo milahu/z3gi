4b2166e12faa52d5001b95e9badf2c265529c2cb
Rick Smetsers <r.smetsers@cs.ru.nl>
2017-06-14 17:00:42 +0200

    Working example for left invariant, unique valued register automata that do not visit the same guard twice.

--- scrap.py
+++ scrap.py
@@ -2,7 +2,7 @@ import collections
 import z3
 
 # states
-n = 2
+n = 3
 State = z3.Datatype('State')
 for i in range(n):
     State.declare('state{0}'.format(i))
@@ -23,62 +23,74 @@ fresh = Register.fresh
 print(registers)
 
 # inputs
-Input = z3.DeclareSort('Input')
-input = z3.Const('input', Input)
-input2 = z3.Const('input2', Input)
+Value = z3.DeclareSort('Value')
+val = z3.Const('val', Value)
+other = z3.Const('other', Value)
 
 RegisterAutomaton = collections.namedtuple('RegisterAutomaton', 'start transition output registers set select store')
 ra = RegisterAutomaton(start=z3.Const('start', State),
-                       transition=z3.Function('transition', State, Input, State),
+                       transition=z3.Function('transition', State, Value, State),
                        output=z3.Function('output', State, z3.BoolSort()),
                        registers=registers,
                        set=z3.Function('set', State, Register, z3.BoolSort()),
-                       select=z3.Function('select', State, Input, Register),
-                       store=z3.Function('store', State, Input, Register))
+                       select=z3.Function('select', State, Value, Register),
+                       store=z3.Function('store', State, Value, Register))
 
 axioms = [z3.ForAll([register],
                     z3.Implies(register != fresh, ra.set(ra.start, register) == False)),
-          z3.ForAll([state, input],
+          z3.ForAll([state, val],
                     z3.And([
                         z3.And(
                         # a (non-fresh) guard on transition implies that it is set in the source
-                        z3.Implies(ra.select(state, input) == register,
+                        z3.Implies(ra.select(state, val) == register,
                                    ra.set(state, register) == True),
                         # an update of a (non-perp) register on a transition implies
                         #  that there is no guard on that register on that transition
-                        z3.Implies(ra.store(state, input) == register,
-                                   ra.select(state, input) == fresh),
+                        z3.Implies(ra.store(state, val) == register,
+                                   ra.select(state, val) == fresh),
                         # if a register is updated is must be used
-                        z3.Implies(ra.store(state, input) == register,
-                                   ra.set(ra.transition(state, input), register) == True),
+                        z3.Implies(ra.store(state, val) == register,
+                                   ra.set(ra.transition(state, val), register) == True),
                         # If a variable is used after a transition, then it was either used before the transition,
                         #  or it stores the parameter carried by the transition
-                        z3.Implies(ra.set(ra.transition(state, input), register) == True,
+                        z3.Implies(ra.set(ra.transition(state, val), register) == True,
                                    z3.Or(ra.set(state, register) == True,
-                                         z3.And(ra.select(state, input) == fresh,
-                                                ra.store(state, input) == register))))
+                                         z3.And(ra.select(state, val) == fresh,
+                                                ra.store(state, val) == register))))
                         for register in registers[1:]]),
-                    patterns = [ra.transition(state, input)]),
-          z3.ForAll([state, input, input2],
-                    z3.Implies(z3.And([input != input2, ra.transition(state, input) != ra.transition(state, input2)]),
-                               ra.select(state, input) != ra.select(state, input2)))]
+                    patterns = [ra.transition(state, val)]),
+          z3.ForAll([state, val, other],
+                    z3.Implies(z3.And([val != other, ra.transition(state, val) != ra.transition(state, other)]),
+                               ra.select(state, val) != ra.select(state, other)))]
 
 
 def _state(aut, access):
     if not access:
         return aut.start
-    return aut.transition(_state(aut, access[:-1]), z3.Const(access[-1], Input))
+    return aut.transition(_state(aut, access[:-1]), access[-1])
+
+def canonical(seq):
+    values = {}
+    i = 0
+    for input in seq:
+        if input not in values:
+            values[input] = z3.Const('val{0}'.format(i), Value)
+            i = i + 1
+    return [values[input] for input in seq]
 
 
-constraints = [ra.output(_state(ra, '11')) == True,
-               ra.output(_state(ra, '10')) == False,
-               ra.output(_state(ra, '110')) == False,
-               ra.output(_state(ra, '111')) == True,
-               ra.output(_state(ra, '11101')) == True,
-               ra.output(_state(ra, '111011')) == True,
+constraints = [ra.output(_state(ra, canonical('11'))) == True,
+               ra.output(_state(ra, canonical('10'))) == False,
+               ra.output(_state(ra, canonical('110'))) == False,
+               ra.output(_state(ra, canonical('111'))) == True,
+               ra.output(_state(ra, canonical('11101'))) == True,
+               ra.output(_state(ra, canonical('111011'))) == True,
 
-               # bijection doesn't work
-               #ra.output(_state(ra, '00')) == True
+               # bijection
+               ra.output(_state(ra, canonical('00'))) == True,
+               ra.output(_state(ra, canonical('88555'))) == True,
+
+               z3.Distinct([z3.Const('val{0}'.format(i), Value) for i in range(2)])
                ]
 
 solver = z3.Solver()
